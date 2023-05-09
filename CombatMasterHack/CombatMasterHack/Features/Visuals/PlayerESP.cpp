50ce7d3626efa1870fd651a14e05a3336b7c2c93
ObamaAteMyKids <72033975+ObamaAteMyKids@users.noreply.github.com>
2023-05-09 20:23:52 +0200

    esp cleanup

--- CombatMasterHack/CombatMasterHack/Features/Visuals/PlayerESP.cpp
+++ CombatMasterHack/CombatMasterHack/Features/Visuals/PlayerESP.cpp
@@ -1,7 +1,7 @@
 #include "../../Includes.hpp"
 
 void PlayerESP::GetBoundingBox(PlayerRoot* player, Box& box)
-{ //not the best way of doing this but good enough
+{
 	if (!player) {
 		Hooks::Game::allPlayers.clear();
 		return;
@@ -23,6 +23,9 @@ void PlayerESP::GetBoundingBox(PlayerRoot* player, Box& box)
 
 void PlayerESP::OnRender()
 {
+	if (PlayerUIManager::GetMyPlayer() == nullptr)
+		Hooks::Game::allPlayers.clear();
+
 	if (!Menu::Vars::esp)
 		return;
 
@@ -47,84 +50,119 @@ void PlayerESP::OnRender()
 
 		auto drawList = ImGui::GetForegroundDrawList();
 
-		//@TODO put these in diff funcs so it doesnt look cluttered af
+		PlayerESP::Features::BoxEsp(currentPlayer, box);
+		PlayerESP::Features::HealthEsp(currentPlayer, box);
+		PlayerESP::Features::FlagEsp(currentPlayer, box);
+		PlayerESP::Features::WeaponEsp(currentPlayer, box);
+	}
+}
 
-		//box
-		auto color = currentPlayer->GetIsVisible() ? ImColor(225, 40, 40, 255) : ImColor(255, 255, 255, 255);
+void PlayerESP::Features::BoxEsp(PlayerRoot* player, Box& box)
+{
+	auto drawList = ImGui::GetForegroundDrawList();
 
-		drawList->AddRect(ImVec2(box.x - 1, box.y - 1), ImVec2(box.x + box.w + 1, box.y + box.h + 1), ImColor(0, 0, 0, 255));
-		drawList->AddRect(ImVec2(box.x, box.y), ImVec2(box.x + box.w, box.y + box.h), color);
-		drawList->AddRect(ImVec2(box.x + 1, box.y + 1), ImVec2(box.x + box.w - 1, box.y + box.h - 1), ImColor(0, 0, 0, 255));
+	auto color = player->GetIsVisible() ? ImColor(225, 40, 40, 255) : ImColor(255, 255, 255, 255);
 
-		//health
-		auto health = currentPlayer->GetPlayerHealth()->GetHealth();
+	drawList->AddRect(ImVec2(box.x - 1, box.y - 1), ImVec2(box.x + box.w + 1, box.y + box.h + 1), ImColor(0, 0, 0, 255));
+	drawList->AddRect(ImVec2(box.x, box.y), ImVec2(box.x + box.w, box.y + box.h), color);
+	drawList->AddRect(ImVec2(box.x + 1, box.y + 1), ImVec2(box.x + box.w - 1, box.y + box.h - 1), ImColor(0, 0, 0, 255));
+}
 
-		Box nBox =
-		{
-			box.x - 2,
-			box.y,
-			1,
-			box.h
-		};
+void PlayerESP::Features::HealthEsp(PlayerRoot* player, Box& box)
+{
+	auto drawList = ImGui::GetForegroundDrawList();
 
-		auto barHeight = (int)((float)health * (float)nBox.h / 100.0f);
-		auto offset = nBox.h - barHeight;
+	if (!player->GetPlayerHealth())
+		return;
 
-		drawList->AddLine(ImVec2(nBox.x, nBox.y + offset), ImVec2(nBox.x, nBox.y + offset + barHeight), ImColor(50, 225, 50, 255));
+	auto health = player->GetPlayerHealth()->GetHealth();
 
-		//flags
-		int flagOffset = 0;
+	Box nBox =
+	{
+		box.x - 2,
+		box.y,
+		1,
+		box.h
+	};
 
-		if (!currentPlayer->GetIsRealPlayer()) {
-			drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(255, 225, 255, 255), "BOT");
-			flagOffset += 12;
-		}
+	auto barHeight = (int)((float)health * (float)nBox.h / 100.0f);
+	auto offset = nBox.h - barHeight;
 
-		if (currentPlayer->GetIsVisible()) {
-			drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(255, 225, 255, 255), "Visible");
-			flagOffset += 12;
-		}
+	drawList->AddLine(ImVec2(nBox.x, nBox.y + offset), ImVec2(nBox.x, nBox.y + offset + barHeight), ImColor(50, 225, 50, 255));
+}
 
-		if (currentPlayer->GetPlayerHealth()->GetIsInvincible()) {
+void PlayerESP::Features::FlagEsp(PlayerRoot* player, Box& box)
+{
+	auto drawList = ImGui::GetForegroundDrawList();
+
+	int flagOffset = 0;
+
+	if (!player->GetIsRealPlayer()) {
+		drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(255, 225, 255, 255), "Bot");
+		flagOffset += 12;
+	}
+
+	if (player->GetIsVisible()) {
+		drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(255, 225, 255, 255), "Visible");
+		flagOffset += 12;
+	}
+
+	if (player->GetPlayerHealth()) {
+
+		if (player->GetPlayerHealth()->GetIsInvincible()) {
 			drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(255, 225, 255, 255), "Invincible");
 			flagOffset += 12;
 		}
 
-		if (currentPlayer->GetPlayerHealth()->GetIsSelfReviving()) {
+		if (player->GetPlayerHealth()->GetIsSelfReviving()) {
 			drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(225, 40, 40, 255), "Self Revive");
 			flagOffset += 12;
 		}
 
-		if (currentPlayer->GetPlayerHealth()->GetIsDowned()) {
+		if (player->GetPlayerHealth()->GetIsDowned()) {
 			drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(215, 190, 35, 255), "Downed");
 			flagOffset += 12;
 		}
 
-		if (currentPlayer->GetPlayerArming()->GetIsBombPlantingOrDefusing()) {
+	}
+
+	if (player->GetPlayerArming()) {
+
+		if (player->GetPlayerArming()->GetIsBombPlantingOrDefusing()) {
 			drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(225, 40, 40, 255), "Plant/Diffuse");
 			flagOffset += 12;
 		}
 
-		if (currentPlayer->GetPlayerArming()->GetIsReloading()) {
+		if (player->GetPlayerArming()->GetIsReloading()) {
 			drawList->AddText(ImVec2(box.x + box.w + 1, box.y + flagOffset), ImColor(215, 190, 35, 255), "Reloading");
 			flagOffset += 12;
 		}
-
-		//weapon+ammo
-		auto weapon = currentPlayer->GetPlayerArming()->GetActiveWeapon();
-		auto isExplosive = weapon->GetWeaponInfo()->GetIsExplosive();
-		auto isMelee = weapon->GetWeaponInfo()->GetIsMeleeWeapon();
-
-		//@TODO: get actual weapon name lolz
-		std::string weaponText = isExplosive ? "Explosive" : isMelee ? "Melee" : "Gun";
-		auto weaponTextSize = ImGui::CalcTextSize(weaponText.c_str());
-
-		std::string ammoText = isExplosive ? "" : isMelee ? "" : std::to_string(weapon->GetChargedAmmo()) + "/" + std::to_string(weapon->GetUnchargedAmmo());
-		auto ammoTextSize = ImGui::CalcTextSize(ammoText.c_str());
-
-		drawList->AddText(ImVec2(box.x + box.w / 2 - (weaponTextSize.x / 2), box.y + box.h + 1), ImColor(255, 255, 255, 255), weaponText.c_str());
-		drawList->AddText(ImVec2(box.x + box.w / 2 - (ammoTextSize.x / 2), box.y + box.h + 13), ImColor(255, 255, 255, 255), ammoText.c_str());
-
-		//@TODO: name esp
 	}
+}
+
+void PlayerESP::Features::WeaponEsp(PlayerRoot* player, Box& box)
+{
+	if (!player->GetPlayerArming())
+		return;
+
+	auto drawList = ImGui::GetForegroundDrawList();
+
+	auto weapon = player->GetPlayerArming()->GetActiveWeapon();
+	auto isExplosive = weapon->GetWeaponInfo()->GetIsExplosive();
+	auto isMelee = weapon->GetWeaponInfo()->GetIsMeleeWeapon();
+
+	//@TODO: get actual weapon name lolz
+	std::string weaponText = isExplosive ? "Explosive" : isMelee ? "Melee" : "Gun";
+	auto weaponTextSize = ImGui::CalcTextSize(weaponText.c_str());
+
+	std::string ammoText = isExplosive ? "" : isMelee ? "" : std::to_string(weapon->GetChargedAmmo()) + "/" + std::to_string(weapon->GetUnchargedAmmo());
+	auto ammoTextSize = ImGui::CalcTextSize(ammoText.c_str());
+
+	drawList->AddText(ImVec2(box.x + box.w / 2 - (weaponTextSize.x / 2), box.y + box.h + 1), ImColor(255, 255, 255, 255), weaponText.c_str());
+	drawList->AddText(ImVec2(box.x + box.w / 2 - (ammoTextSize.x / 2), box.y + box.h + 13), ImColor(255, 255, 255, 255), ammoText.c_str());
+}
+
+void PlayerESP::Features::NameEsp(PlayerRoot* player, Box& box)
+{
+	auto drawList = ImGui::GetForegroundDrawList();
 }
--- CombatMasterHack/CombatMasterHack/Features/Visuals/PlayerESP.hpp
+++ CombatMasterHack/CombatMasterHack/Features/Visuals/PlayerESP.hpp
@@ -12,4 +12,13 @@ namespace PlayerESP
 {
 	void OnRender();
 	void GetBoundingBox(PlayerRoot* player, Box& box);
+
+	namespace Features
+	{
+		void BoxEsp(PlayerRoot* player, Box& box);
+		void HealthEsp(PlayerRoot* player, Box& box);
+		void FlagEsp(PlayerRoot* player, Box& box);
+		void WeaponEsp(PlayerRoot* player, Box& box);
+		void NameEsp(PlayerRoot* player, Box& box);
+	}
 }
